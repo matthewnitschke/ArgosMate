59bc3ae41bcf0ef0d2ea4b07ebecb1b2bfc1c37e
Matthew Nitschke <matthewcnitschke@gmail.com>
2026-04-21 20:19:00 -0600

    icon working

--- Sources/ArgosMate/main.swift
+++ Sources/ArgosMate/main.swift
@@ -1,12 +1,26 @@
 import AppKit
 
 let app = NSApplication.shared
+app.setActivationPolicy(.accessory)
+
 let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
 
 if let button = statusItem.button {
-    if let iconURL = Bundle.main.url(forResource: "icon", withExtension: "svg"),
-       let iconImage = NSImage(contentsOf: iconURL) {
-        button.image = iconImage
+    let bundle = Bundle.main
+    print("Bundle:", bundle.bundlePath)
+    print("EXE:", bundle.executablePath ?? "")
+
+    let exePath = bundle.executablePath ?? ""
+    let exeURL = URL(fileURLWithPath: exePath)
+    let bundlePath = exeURL.deletingLastPathComponent()
+    let resourcesPath = bundlePath.appendingPathComponent("ArgosMate_ArgosMate.bundle")
+    let iconPath = resourcesPath.appendingPathComponent("icon.svg").path
+    print("Icon path:", iconPath)
+
+    if let image = NSImage(contentsOfFile: iconPath) {
+        image.isTemplate = true
+        image.size = NSSize(width: 22, height: 22)
+        button.image = image
     } else {
         button.image = NSImage(systemSymbolName: "ant", accessibilityDescription: "ArgosMate")
     }
