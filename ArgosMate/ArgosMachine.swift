18bbf8d0451ec35c1e904ad54b42483af515e176
Matthew Nitschke <matthewcnitschke@gmail.com>
2026-04-26 08:26:28 -0600

    updates

--- ArgosMate/ArgosMachine.swift
+++ ArgosMate/ArgosMachine.swift
@@ -2,11 +2,17 @@ import Foundation
 import CoreBluetooth
 import Combine
 
+enum FluidLevel: UInt32 {
+    case ok = 0
+    case needsWater = 1
+}
+
 let serviceUUID = CBUUID(string: "6a521c59-55b5-4384-85c0-6534e63fb09e")
 let setPointUUID = CBUUID(string: "6a521c60-55b5-4384-85c0-6534e63fb09e")
 let boilerCurrentUUID = CBUUID(string: "6a521c61-55b5-4384-85c0-6534e63fb09e")
 let boilerTargetUUID = CBUUID(string: "6a521c66-55b5-4384-85c0-6534e63fb09e")
 let groupheadTempUUID = CBUUID(string: "6A521C62-55B5-4384-85C0-6534E63FB09E")
+let fluidLevelUUID = CBUUID(string: "6A521C63-55B5-4384-85C0-6534E63FB09E")
 
 final class ArgosMachine: NSObject, ObservableObject {
     @Published var isConnected: Bool = false
@@ -14,7 +20,7 @@ final class ArgosMachine: NSObject, ObservableObject {
     @Published var boilerCurrent: Double = 0
     @Published var boilerTarget: Double = 0
     @Published var groupheadTemp: Double = 0
-    @Published var waterStatus: String = "OK"
+    @Published var fluidLevel: FluidLevel = .ok
 
     private var centralManager: CBCentralManager?
     private var peripheral: CBPeripheral?
@@ -60,6 +66,7 @@ extension ArgosMachine: CBCentralManagerDelegate {
             self?.isConnected = false
             self?.boilerCurrent = 0
             self?.groupheadTemp = 0
+            self?.fluidLevel = .ok
         }
         central.scanForPeripherals(withServices: [serviceUUID], options: nil)
     }
@@ -69,7 +76,7 @@ extension ArgosMachine: CBPeripheralDelegate {
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         guard let services = peripheral.services else { return }
         for service in services {
-            peripheral.discoverCharacteristics([setPointUUID, boilerCurrentUUID, boilerTargetUUID], for: service)
+            peripheral.discoverCharacteristics([setPointUUID, boilerCurrentUUID, boilerTargetUUID, fluidLevelUUID], for: service)
         }
     }
 
@@ -81,21 +88,28 @@ extension ArgosMachine: CBPeripheralDelegate {
     }
 
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        guard let data = characteristic.value, data.count >= 8 else { return }
-        let value = data.withUnsafeBytes { $0.load(as: Double.self) }
+        guard let data = characteristic.value else { return }
 
         DispatchQueue.main.async { [weak self] in
             switch characteristic.uuid {
-            case setPointUUID:
-                self?.setPoint = value
-            case boilerCurrentUUID:
-                self?.boilerCurrent = value
-            case boilerTargetUUID:
-                self?.boilerTarget = value
-            case groupheadTempUUID:
-                self?.groupheadTemp = value
+            case fluidLevelUUID:
+                let rawValue = data.withUnsafeBytes { $0.load(as: UInt32.self) }
+                self?.fluidLevel = FluidLevel(rawValue: rawValue) ?? .ok
             default:
-                break
+                guard data.count >= 8 else { return }
+                let value = data.withUnsafeBytes { $0.load(as: Double.self) }
+                switch characteristic.uuid {
+                case setPointUUID:
+                    self?.setPoint = value
+                case boilerCurrentUUID:
+                    self?.boilerCurrent = value
+                case boilerTargetUUID:
+                    self?.boilerTarget = value
+                case groupheadTempUUID:
+                    self?.groupheadTemp = value
+                default:
+                    break
+                }
             }
         }
     }
