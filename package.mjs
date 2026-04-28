bd9c6829275f21fb4fb929995df37c08c7d9f81a
Matthew Nitschke <matthewcnitschke@gmail.com>
2026-04-27 22:10:35 -0600

    package script

---
+++ package.mjs
@@ -0,0 +1,52 @@
+#!/usr/bin/env zx
+
+/**
+ * package.mjs runs the necessary steps to generate a packaged app for EmberMate
+ * 
+ * Dependencies:
+ * - zx: https://google.github.io/zx/
+ * - create-dmg: https://github.com/create-dmg/create-dmg
+ * 
+ * Execution: `$ ./package.mjs`
+ */
+
+const currentVersion = (await $`grep -m 1 "MARKETING_VERSION = " ./ArgosMate.xcodeproj/project.pbxproj | sed -E 's/.*MARKETING_VERSION = ([0-9.]+);.*/\\1/'`).text().trim()
+
+const shouldUpdate = (await question(`Current project version is ${currentVersion}. Would you like to update this? (Y/n): `)).trim().toLowerCase()
+if (shouldUpdate == '' || shouldUpdate == 'y') {
+  const newVersion = (await question(`Enter new version: `)).trim()
+
+  await $`sed -i '' "s/MARKETING_VERSION = .*;/MARKETING_VERSION = ${newVersion};/" ./ArgosMate.xcodeproj/project.pbxproj`
+}
+
+await spinner('Building Project', () => $`
+xcodebuild \
+    -project "ArgosMate.xcodeproj" \
+    -scheme "ArgosMate" \
+    -destination "generic/platform=macOS" \
+    archive \
+    CODE_SIGN_IDENTITY="-" \
+    -archivePath "build/ArgosMate.xcarchive"
+`)
+
+await spinner('Updating exportOptions', () => $`
+plutil -convert xml1 - -o "exportOptions.plist" << EOF
+    {
+        "destination": "export",
+        "method": "mac-application"
+    }
+EOF
+`)
+
+await spinner('Exporting Archive', () => $`
+xcodebuild \
+    -exportArchive \
+    -archivePath "build/ArgosMate.xcarchive" \
+    -exportPath "build" \
+    -exportOptionsPlist "exportOptions.plist"
+`)
+
+// Remove existing DMGs if they exists
+await $`rm -f ./ArgosMate*.dmg`
+
+await $`create-dmg --dmg-title='ArgosMate' 'build/ArgosMate.app' ./`
